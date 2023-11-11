5710324084fcb6687b97cb2dd5971b182626a3ce
migueflorez10 <68928440+migueflorez10@users.noreply.github.com>
2023-11-11 02:29:34 -0500

    creation of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LinkButtonComponent } from './link-button/link-button.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { HeroComponent } from './hero/hero.component';
+import { SectionTitleComponent } from './section-title/section-title.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkButtonComponent,
+    ParagraphComponent,
+    HeroComponent,
+    SectionTitleComponent,
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
