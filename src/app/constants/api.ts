93918155404a79d37a915c03b091bcac0dea686d
Brahiam E. Quintero T <brahiamquintero@hotmail.com>
2023-11-24 02:22:44 -0500

    Persistence Improved

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -6,6 +6,7 @@ export const ENDPOINTS = {
   LOGIN: `${API_URL}/auth/login`,
   LOGIN_SUPPORT: `${API_URL}/auth/support-staff-login`,
   POST_ANSWERS: `${API_URL}/questionary/save-answers`,
+  GET_ANSWERS: `${API_URL}/questionary`,
   POST_FORM_STATE: `${API_URL}/questionary/update-status`,
   POST_GEN_SUPPORT_REPORT: `${API_URL}/reports/generate`,
   POST_FEEDBACK: `${API_URL}/UNDEFINED`,
--- src/app/services/form/form.service.ts
+++ src/app/services/form/form.service.ts
@@ -4,24 +4,33 @@ import { ENDPOINTS } from 'src/app/constants/api';
 import {
   CensusFormAnswer,
   CensusResponseRequest,
+  FormStateRequest,
 } from 'src/app/interfaces/form';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormService {
-  constructor(private http: HttpClient) {}
+  ecn: string;
+
+  constructor(private http: HttpClient) {
+    this.ecn = 'ECN1';
+  }
 
   sendAnswers(answers: CensusFormAnswer[]) {
     let body: CensusResponseRequest = {
       //TODO
-      ecn: '1234567890',
+      ecn: this.ecn,
       answers: answers,
     };
     return this.http.post(ENDPOINTS.POST_ANSWERS, { answers });
   }
 
-  setCompletedFormState(formId: string) {
+  getAnswers() {
+    return this.http.get(`${ENDPOINTS.GET_ANSWERS}/${this.ecn}`);
+  }
+
+  setCompletedFormState(formId: FormStateRequest) {
     return this.http.post(ENDPOINTS.POST_FORM_STATE, { formId });
   }
 
